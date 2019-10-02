061dde8bad96ba595ec483459c268c4fde638ace
Yanis Urbis <yanis.urbis@gmail.com>
2019-10-02 12:48:30 +0300

    Update default text
    Co-Authored-By: Antony <acron1@gmail.com>

--- client/src/wh/logged_in/personalised_jobs/views.cljs
+++ client/src/wh/logged_in/personalised_jobs/views.cljs
@@ -33,5 +33,5 @@
         :else (case type-of-jobs
                 :recommended [[:p "Add some skills and preffered locations to your profile to see recommendations."]]
                 :liked [[:p "Click on some " [icon "like" :class "like red-fill"] " to save jobs you like."]]
-                :applied [[:p "Applied for some jobs in our " [link "job board" :jobsboard :class "a--underlined"] "."]]
+                :applied [[:p "You haven't applied for any jobs yet... " [link "What are you waiting for?" :jobsboard :class "a--underlined"] "."]]
                 [[:p "No jobs found."]])))))
