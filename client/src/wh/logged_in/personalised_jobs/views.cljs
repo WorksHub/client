85dcf109ea06d409b22e7f376e35c5d330a83d1d
Alex-Bakic <alexbakic@outlook.com>
2019-11-14 11:58:59 +0000

    tweaked job-page data to map instead of tuple. :applied doesn't need an on-close as nothing get's re-rendered. The missing on-close is handled in page, where it can be set to nil if no on-close key is provided.

--- client/src/wh/logged_in/personalised_jobs/views.cljs
+++ client/src/wh/logged_in/personalised_jobs/views.cljs
@@ -9,19 +9,17 @@
     [wh.logged-in.personalised-jobs.subs :as subs]
     [wh.subs :refer [<sub]]))
 
-;; first element is the on-close map, the second element is the relevant job pool , and the third element is the message if page would be empty.
 (defn job-type-data
   [type-of-jobs]
   (case type-of-jobs
-    :recommended [{:on-close :reload-recommended}
-                  [::subs/recommended-jobs]
-                  [[:p "Add some skills and preffered locations to your profile to see recommendations."]]]
-    :liked [{:on-close :reload-liked}
-            [::subs/liked-jobs]
-            [[:p "Click on some " [icon "like" :class "like red-fill"] " to save jobs you like."]]]
-    :applied [{:on-close :none}
-              [::subs/applied-jobs]
-              [[:p "You haven't applied for any jobs yet... " [link "What are you waiting for?" :jobsboard :class "a--underlined"] "."]]]))
+    :recommended {:on-close :reload-recommended
+                  :sub [::subs/recommended/jobs]
+                  :message [[:p "Add some skills and preferred locations to your profile to see recommendations."]]}
+    :liked       {:on-close :reload-liked
+                  :sub [::subs/liked-jobs]
+                  :message [[:p "Click on some " [icon "like" :class "like red-fill"] " to save jobs you like."]]}
+    :applied     {:sub [::subs/applied-jobs]
+                  :message [[:p "You haven't applied for any jobs yet... " [link "What are you waiting for?" :jobsboard :class "a--underlined"] "."]]}))
 
 (defn page [type-of-jobs]
   (into
@@ -31,11 +29,10 @@
       (when (= type-of-jobs :recommended)
         [:div.has-bottom-margin
          [link [:button.button "Improve recommendations"] :improve-recommendations :class "level-item"]])]]
-    ;; data stops us from having to do three case checks , by putting all relevant data per keyword into a vector.
-    (let [data (job-type-data type-of-jobs)
-          on-close (first data)
-          subscription (second data)
-          message (last data)
+    (let [{on-close :on-close
+           subscription :sub
+           message :message
+           :or {:on-close nil}} (job-type-data type-of-jobs)
           parts (partition-all 3 (<sub subscription))
           has-applied? (some? (<sub [:user/applied-jobs]))]
       (cond
@@ -45,11 +42,10 @@
                                          [:div.column.is-4
                                           [job-card job (merge {:user-has-applied? has-applied?
                                                                 :logged-in? true}
-                                                               on-close)]]))))
+                                                               (when on-close
+                                                                 {:on-close on-close}))]]))))
                           [:div.columns.is-centered.load-more-section
                            [:div.column.is-4.has-text-centered
                             (when (<sub [::subs/show-load-more?])
                               [:button.button {:on-click #(dispatch [::events/load-more type-of-jobs])} "Load more Jobs"])]])
-        :else (if message
-                message
-                [[:p "No jobs found."]])))))
+        :else (or message [[:p "No jobs found."]])))))
