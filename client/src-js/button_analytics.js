d7bb050510369bebac90180de4254eac97cc0af6
Yanis Urbis <yanis.urbis@gmail.com>
2020-03-18 14:09:36 +0000

    Yanisurbis/ch3799/add specific analytics onto the buttons onready exist
    * Add script
    
    * Add interop function
    
    * Call js function on page load and update
    
    * Update script
    
    * Revert "Call js function on page load and update"
    
    This reverts commit 9e895f39f029b869577b0babe1d808aec41cdd1d.
    
    * Revert "Add interop function"
    
    This reverts commit 0ea3200d3020eaf7042a57f76bb5ee998ed98a74.
    
    * Remove console.log
    
    Co-authored-by: Antony <acron1@gmail.com>

---
+++ client/src-js/button_analytics.js
@@ -0,0 +1,23 @@
+(function addButtonsAnalytics() {
+  function sendAnalytics(id) {
+    let eventName = "Button Pressed";
+    let properties = { id };
+
+    sendServerAnalytics({
+      type: "track",
+      payload: { "event-name": eventName, properties }
+    });
+    submitAnalyticsTrack(eventName, properties);
+  }
+
+  const isButton = elm => elm.tagName === "BUTTON";
+  const hasId = elm => elm.id !== "";
+  const hasTrackClass = elm => elm.classList.contains("track-click");
+
+  document.addEventListener("click", evt => {
+    const elm = evt.target;
+    if (hasId(elm) && (isButton(elm) || hasTrackClass(elm))) {
+      sendAnalytics(elm.id)
+    }
+  })
+})();
