24979fb7c6642524f075b43b9671d2a2c7166353
fricze <jonki.web@gmail.com>
2020-06-05 13:58:50 +0000

    Set proper case for .cljc filenames created by cljc-css-loader
    Co-authored-by: fricze <andrzej.fricze@gmail.com>

--- cljc-css-loader/index.js
+++ cljc-css-loader/index.js
@@ -4,72 +4,75 @@
 // It doesn't change it's sources anyhow, so you may use it after css-loader
 // and still consume css-loader artifacts in next loaders
 
-const fs = require("fs");
-const { Parser } = require("acorn");
-const { getOptions } = require("loader-utils");
+const fs = require('fs');
+const { Parser } = require('acorn');
+const { getOptions } = require('loader-utils');
 
 const last = x => x[x.length - 1];
 
-const snakeCaseToKebabCase = str => {
-    return str.replace(/_/g, "-");
-};
+const snakeCaseToKebabCase = str => str.replace(/_/g, '-');
+const kebabCaseToSnakeCase = str => str.replace(/-/g, '_');
 
 const astToDefs = ast => {
-    let defs;
+  let defs;
 
-    ast.body.forEach(node => {
-        if (node.type === "ExpressionStatement" && node.expression.left) {
-            const { right, left } = node.expression;
-            const { object, property } = left;
+  ast.body.forEach(node => {
+    if (node.type === 'ExpressionStatement' && node.expression.left) {
+      const { right, left } = node.expression;
+      const { object, property } = left;
 
-            if (
-                object &&
-                object.name === "exports" &&
-                property &&
-                property.name === "locals"
-            ) {
-                defs = right.properties
-                    .map(property => {
-                        const key = property.key.value;
-                        const value = property.value.value;
+      if (
+        object &&
+        object.name === 'exports' &&
+        property &&
+        property.name === 'locals'
+      ) {
+        defs = right.properties
+          .map(property => {
+            const key = property.key.value;
+            const value = property.value.value;
 
-                        const cljsDef = `(def ${key} "${value}")`;
+            const cljsDef = `(def ${key} "${value}")`;
 
-                        return cljsDef;
-                    })
-                    .join("\n");
-            }
-        }
-    });
+            return cljsDef;
+          })
+          .join('\n');
+      }
+    }
+  });
 
-    return defs;
+  return defs;
 };
 
 function transform(source) {
-    const MyParser = Parser.extend();
-    const ast = MyParser.parse(source);
+  const MyParser = Parser.extend();
+  const ast = MyParser.parse(source);
 
-    const name = last(this.resourcePath.split("/"));
+  const name = last(this.resourcePath.split('/'));
 
-    const options = getOptions(this);
-    const defs = astToDefs(ast);
+  const options = getOptions(this);
+  const defs = astToDefs(ast);
 
-    const [file, ext] = name.split(".");
-    const fileContent = `(ns ${options.nsPrefix || "styles"}.${snakeCaseToKebabCase(
-        file
-    )}) \n`.concat(defs);
+  const [file, ext] = name.split('.');
 
-    const path = options.path || "./styles";
+  const kebabFileName = snakeCaseToKebabCase(file);
+  const nsPrefix = options.nsPrefix || 'styles';
 
-    if (!fs.existsSync(path)) {
-        fs.mkdirSync(path);
-    }
+  const fileContent = `(ns ${nsPrefix}.${kebabFileName})\n`.concat(defs);
 
-    fs.writeFile(`${path}/${file}.cljc`, fileContent, function (err) {
-        if (err) throw err;
-    });
+  const path = options.path || './styles';
 
-    return source;
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+
+  const snakeFileName = kebabCaseToSnakeCase(file);
+
+  fs.writeFile(`${path}/${snakeFileName}.cljc`, fileContent, function(err) {
+    if (err) throw err;
+  });
+
+  return source;
 }
 
 module.exports = transform;
