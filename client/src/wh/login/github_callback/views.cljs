70259e4f23b0d3ba1974fd40fa7416c0453204cb
Antony Woods <acron1@gmail.com>
2020-07-29 08:47:22 +0000

    Make applicable changes to GitHub auth to avoid deprecated techniques
    * Fix error when user with applications logged in user GH
    
    * Remove query param auth and update various fns to use relevant client fns
    
    * Remove debug hook
    
    * Bump
    
    * Use keys
    
    * Add comment
    
    * Add another comment

--- client/src/wh/login/github_callback/views.cljs
+++ client/src/wh/login/github_callback/views.cljs
@@ -2,8 +2,8 @@
   (:require
     [wh.components.conversation.views :refer [error-message button]]
     [wh.components.icons :refer [icon]]
-    [wh.login.github-callback.subs :as subs]
     [wh.login.github-callback.db :as github-callback]
+    [wh.login.github-callback.subs :as subs]
     [wh.login.views :as login-views]
     [wh.subs :refer [<sub]]))
 
@@ -11,11 +11,11 @@
   [:div.register-container
    [:div.columns.full-height
     [:div.column.is-half.is-offset-one-quarter.full-height
-     (let [error (<sub [::subs/error])
+     (let [error              (<sub [::subs/error])
            already-connected? (github-callback/already-connected? error)
-           msg (if already-connected?
-                 "This Github account is connected to another user profile."
-                 "We couldn't fetch your Github information.")]
+           msg                (if already-connected?
+                                "This GitHub account is connected to another user profile."
+                                "We couldn't fetch your GitHub information.")]
        (when error
          [:div.conversation
           [error-message msg]
