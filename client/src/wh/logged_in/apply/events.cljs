5ceed772890c1588d40ac24691914d51f9a2dfd3
el arbaoui oussama <elarbaouioussama@gmail.com>
2021-07-30 09:47:56 +0000

    Accept tracking when creating a user
    * Accept tracking when creating a user
    
    * empty commit

--- client/src/wh/logged_in/apply/events.cljs
+++ client/src/wh/logged_in/apply/events.cljs
@@ -46,15 +46,15 @@
 ;; START JOB APPLICATION ─────────────────────────────────────────────────────────────────────────────
 
 (reg-event-fx
- :apply/start-apply-for-job
- db/default-interceptors
- (fn [{db :db} [job apply-source]]
-   (let [job (assoc job :apply-source apply-source)
-         logged-in? (boolean (get-in db [:wh.user.db/sub-db :wh.user.db/id]))]
-     {:db              (assoc-in db [::apply/sub-db ::apply/job] job)
-      :dispatch-n      [(if logged-in? [::check-name] [::check-email]) 
-                        [::fetch-job-company-details job]]
-      :analytics/track ["Job Application Started" job]})))
+  :apply/start-apply-for-job
+  db/default-interceptors
+  (fn [{db :db} [job apply-source]]
+    (let [job        (assoc job :apply-source apply-source)
+          logged-in? (boolean (get-in db [:wh.user.db/sub-db :wh.user.db/id]))]
+      {:db              (assoc-in db [::apply/sub-db ::apply/job] job)
+       :dispatch-n      [(if logged-in? [::check-name] [::check-email])
+                         [::fetch-job-company-details job]]
+       :analytics/track ["Job Application Started" job]})))
 
 (reg-event-fx
   ::fetch-job-company-details
@@ -69,8 +69,8 @@
   apply-interceptors
   (fn [db [{{{company :company} :job} :data}]]
     (assoc db
-           ::apply/company-managed? (:managed company)
-           ::apply/company-name     (:name company))))
+      ::apply/company-managed? (:managed company)
+      ::apply/company-name (:name company))))
 
 ;; CV ─────────────────────────────────────────────────────────────────────────────
 
@@ -80,9 +80,9 @@
   (fn [{db :db} [_]]
     (if (user/has-cv? db)
       {:dispatch [::check-skills]}
-      {:db (-> db
-               (apply/update-current-step :step/cv-upload)
-               (apply/update-taken-steps :step/cv-upload))
+      {:db       (-> db
+                     (apply/update-current-step :step/cv-upload)
+                     (apply/update-taken-steps :step/cv-upload))
        :dispatch scroll-to-bottom})))
 
 (reg-event-db
@@ -127,9 +127,9 @@
   db/default-interceptors
   (fn [{db :db} [_]]
     (when-not (user/valid-email? db)
-      {:db (-> db
-               (apply/update-current-step :step/email)
-               (apply/update-taken-steps :step/email))
+      {:db       (-> db
+                     (apply/update-current-step :step/email)
+                     (apply/update-taken-steps :step/email))
        :dispatch scroll-to-bottom})))
 
 ;; Here we assume that the user has an account already
@@ -139,21 +139,21 @@
   ::send-magic-link
   db/default-interceptors
   (fn [{db :db} [email]]
-    (let [slug (get-in db [:wh.db/page-params :slug])
+    (let [slug         (get-in db [:wh.db/page-params :slug])
           apply-source (get-in db [:wh.db/query-params "apply_source"])
-          base-params {:interaction 1
-                       :apply       true}
+          base-params  {:interaction 1
+                        :apply       true}
           redirect-url (r/path :job
                                :params {:slug slug}
                                :query-params (if apply-source
-                                                (merge base-params {:apply-source apply-source})
-                                                base-params))]
-      {:db       (apply/set-loading db)
-       :graphql  {:query login-events/magic-link-mutation
-                  :variables {:email email
+                                               (merge base-params {:apply-source apply-source})
+                                               base-params))]
+      {:db      (apply/set-loading db)
+       :graphql {:query      login-events/magic-link-mutation
+                 :variables  {:email    email
                               :redirect redirect-url}
-                  :on-success [::send-magic-link-success email]
-                  :on-failure [::send-magic-link-failure email]}})))
+                 :on-success [::send-magic-link-success email]
+                 :on-failure [::send-magic-link-failure email]}})))
 
 ;; Refresh the page with apply=true and interaction=1 and apply_source
 ;; The user shouldn't have to do the email step again
@@ -173,11 +173,11 @@
     ;; If sending the magic link failed because the user doesn't have an account
     ;; we move to the name step, so that the user can enter his name.
     (if (common-user/no-user-found-for-email? resp)
-      {:db (-> db
-               (apply/unset-loading)
-               (user/update-email email))
+      {:db       (-> db
+                     (apply/unset-loading)
+                     (user/update-email email))
        :dispatch [::check-name]}
-       ;; In case the failure is due to a reason other than email not found, show an error to the user
+      ;; In case the failure is due to a reason other than email not found, show an error to the user
       {:db (-> db
                (apply/unset-loading)
                (assoc-in [::apply/sub-db ::apply/email-magic-link-failed?] true))})))
@@ -187,14 +187,15 @@
   ::create-user
   db/default-interceptors
   (fn [{db :db} _]
-    {:db       (apply/set-loading db)
-     :graphql  {:query register-events/create-user-mutation
-                :variables {:create_user {:email (get-in db [::user/sub-db ::user/email])
-                                          :name  (get-in db [::user/sub-db ::user/name])
-                                          :consented (.toISOString (js/Date.))}
-                            :force_unapproved true}
-                :on-success [::create-user-success]
-                :on-failure [::create-user-failure]}}))
+    {:db                          (apply/set-loading db)
+     :graphql                     {:query      register-events/create-user-mutation
+                                   :variables  {:create_user      {:email     (get-in db [::user/sub-db ::user/email])
+                                                                   :name      (get-in db [::user/sub-db ::user/name])
+                                                                   :consented (.toISOString (js/Date.))}
+                                                :force_unapproved true}
+                                   :on-success [::create-user-success]
+                                   :on-failure [::create-user-failure]}
+     :analytics/agree-to-tracking true}))
 
 ;; When a user is created successfully we can continue the application process, 
 ;; no need to refresh the page, because we already have the infos we need
@@ -204,10 +205,10 @@
   (fn [{db :db} [resp]]
     (let [user (get-in resp [:data :create_user])]
       {:db     (-> db
-               (assoc-in [::user/sub-db ::user/id] (:id user))
-               ;; We set the user consented to prevent the agreement modal from showing up
-               (assoc-in [::user/sub-db ::user/consented] (:consented user))
-               (apply/unset-loading))
+                   (assoc-in [::user/sub-db ::user/id] (:id user))
+                   ;; We set the user consented to prevent the agreement modal from showing up
+                   (assoc-in [::user/sub-db ::user/consented] (:consented user))
+                   (apply/unset-loading))
        :reload true})))
 
 (reg-event-fx
@@ -226,23 +227,23 @@
   (fn [{db :db} [_]]
     (if (user/has-full-name? db)
       {:dispatch [::check-current-location]}
-      {:db (-> db
-               (apply/update-current-step :step/name)
-               (apply/update-taken-steps :step/name))
+      {:db       (-> db
+                     (apply/update-current-step :step/name)
+                     (apply/update-taken-steps :step/name))
        :dispatch scroll-to-bottom})))
 
 (reg-event-fx
   ::update-name
   db/default-interceptors
   (fn [{db :db} [name]]
-    (if (user/id db) ;; If we already have a user
+    (if (user/id db)                                        ;; If we already have a user
       {:db      (apply/set-loading db)
        :graphql {:query      graphql/update-user-mutation--name
                  :variables  {:update_user {:id   (user/id db)
                                             :name name}}
                  :on-success [::update-name-success]
                  :on-failure [::update-name-failure]}}
-      {:db (user/update-name db name)
+      {:db       (user/update-name db name)
        :dispatch [::create-user]})))
 
 (reg-event-fx
@@ -308,8 +309,8 @@
 (def set-application-cover-letter-mutation
   {:venia/operation {:operation/type :mutation
                      :operation/name "set_application_cover_letter"}
-   :venia/variables [{:variable/name "job_id"       :variable/type :String!}
-                     {:variable/name "user_id"      :variable/type :ID!}
+   :venia/variables [{:variable/name "job_id" :variable/type :String!}
+                     {:variable/name "user_id" :variable/type :ID!}
                      {:variable/name "cover_letter" :variable/type :cover_letter_input!}]
    :venia/queries   [[:set_application_cover_letter {:job_id       :$job_id
                                                      :user_id      :$user_id
@@ -317,10 +318,10 @@
                       [:status]]]})
 
 (reg-event-db
- ::add-cover-letter
- db/default-interceptors
- (fn [db _]
-   (apply/update-current-step db :step/cover-letter)))
+  ::add-cover-letter
+  db/default-interceptors
+  (fn [db _]
+    (apply/update-current-step db :step/cover-letter)))
 
 (reg-event-db
   ::discard-cover-letter
@@ -339,7 +340,7 @@
   db/default-interceptors
   (fn [{db :db} [name {url :url hash :hash}]]
     {:dispatch [::set-cover-letter {:file {:name name :url url :hash hash}}]
-     :db (assoc-in db [::apply/sub-db ::apply/cover-letter-upload-failed?] false)}))
+     :db       (assoc-in db [::apply/sub-db ::apply/cover-letter-upload-failed?] false)}))
 
 (reg-event-db
   ::cover-letter-upload-failure
@@ -385,9 +386,9 @@
   (fn [{db :db} [_]]
     (if (user/has-current-location? db)
       {:dispatch [::check-cv]}
-      {:db (-> db
-               (apply/update-current-step :step/current-location)
-               (apply/update-taken-steps :step/current-location))
+      {:db       (-> db
+                     (apply/update-current-step :step/current-location)
+                     (apply/update-taken-steps :step/current-location))
        :dispatch scroll-to-bottom})))
 
 (reg-event-fx
@@ -446,15 +447,15 @@
 ;; APPLICATION ─────────────────────────────────────────────────────────────────────────────
 
 (defquery check-application-query
-  {:venia/operation {:operation/type :query
-                     :operation/name "check_application"}
-   :venia/variables [{:variable/name "id"
-                      :variable/type :String}
-                     {:variable/name "slug"
-                      :variable/type :String}]
-   :venia/queries   [[:check_application {:id :$id
-                                          :slug :$slug}
-                      [:check_status :reason]]]})
+          {:venia/operation {:operation/type :query
+                             :operation/name "check_application"}
+           :venia/variables [{:variable/name "id"
+                              :variable/type :String}
+                             {:variable/name "slug"
+                              :variable/type :String}]
+           :venia/queries   [[:check_application {:id   :$id
+                                                  :slug :$slug}
+                              [:check_status :reason]]]})
 
 (reg-event-fx
   ::check-application
@@ -494,9 +495,9 @@
   (fn [{db :db} [_]]
     (if (user/has-skills? db)
       {:dispatch [::apply]}
-      {:db (-> db
-               (apply/update-current-step :step/skills)
-               (apply/update-taken-steps :step/skills))
+      {:db       (-> db
+                     (apply/update-current-step :step/skills)
+                     (apply/update-taken-steps :step/skills))
        :dispatch scroll-to-bottom})))
 
 (reg-event-fx
@@ -506,10 +507,10 @@
     {:graphql {:query      graphql/update-user-mutation--skills
                :variables  {:update_user
                             (keywords/transform-keys {:id     (user/id db)
-                                                  :skills (->> db
-                                                               apply/sub-db
-                                                               apply/selected-skills
-                                                               (map #(hash-map :name %)))})}
+                                                      :skills (->> db
+                                                                   apply/sub-db
+                                                                   apply/selected-skills
+                                                                   (map #(hash-map :name %)))})}
                :on-success [::update-skills-success]
                :on-failure [::update-skills-failure]}
      :db      (-> db
@@ -520,9 +521,9 @@
   ::update-skills-success
   db/default-interceptors
   (fn [{db :db} [resp]]
-    {:db (-> db
-             (user/update-skills (get-in resp [:data :update_user :skills]))
-             apply/unset-loading)
+    {:db       (-> db
+                   (user/update-skills (get-in resp [:data :update_user :skills]))
+                   apply/unset-loading)
      :dispatch [::check-skills]}))
 
 (reg-event-db
@@ -542,17 +543,17 @@
 ;; APPLY ─────────────────────────────────────────────────────────────────────────────
 
 (defquery apply-mutation
-  {:venia/operation {:operation/type :mutation
-                     :operation/name "Apply"}
-   :venia/variables [{:variable/name "id"
-                      :variable/type :String}
-                     {:variable/name "slug"
-                      :variable/type :String}
-                     {:variable/name "applySource"
-                      :variable/type :String}]
-   :venia/queries   [[:apply {:id          :$id
-                              :slug        :$slug
-                              :applySource :$applySource}]]})
+          {:venia/operation {:operation/type :mutation
+                             :operation/name "Apply"}
+           :venia/variables [{:variable/name "id"
+                              :variable/type :String}
+                             {:variable/name "slug"
+                              :variable/type :String}
+                             {:variable/name "applySource"
+                              :variable/type :String}]
+           :venia/queries   [[:apply {:id          :$id
+                                      :slug        :$slug
+                                      :applySource :$applySource}]]})
 
 (reg-event-fx
   ::apply
@@ -571,14 +572,14 @@
   apply-interceptors
   (fn [{db :db} [success? resp]]
     (cond-> {:db (assoc db
-                        ::apply/submit-success? success?
-                        ::apply/updating? false
-                        ::apply/error (when-not success?
-                                        (util/gql-errors->error-key resp)))}
-      success? (assoc :dispatch-n [[::check-visa]
-                                   [:wh.job.events/set-applied]
-                                   [::init-application-pixels]]
-                      :analytics/track ["Job Applied" (::apply/job db)]))))
+                   ::apply/submit-success? success?
+                   ::apply/updating? false
+                   ::apply/error (when-not success?
+                                   (util/gql-errors->error-key resp)))}
+            success? (assoc :dispatch-n [[::check-visa]
+                                         [:wh.job.events/set-applied]
+                                         [::init-application-pixels]]
+                            :analytics/track ["Job Applied" (::apply/job db)]))))
 
 (reg-event-fx
   ::init-application-pixels
